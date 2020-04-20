573f5477a5302971bf749ac775f5527010acf979
Warren Goble <warrengoble@users.noreply.github.com>
2020-04-19 17:47:45 -0700

    Remove JS export
    Added creator and license information to exported JSON

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const fs = require("fs");
 const xml2js = require("xml2js");
-const { stringify } = require("javascript-stringify");
 const axios = require("axios");
 
 const url =
   "https://upload.wikimedia.org/wikipedia/commons/5/59/Usa_counties_large.svg";
 
-const processData = data => {
+const processData = (data) => {
   const parser = new xml2js.Parser();
 
   parser.parseString(data, (err, result) => {
@@ -20,88 +19,70 @@ const processData = data => {
         $: { width: mapWidth, height: mapHeight } = {},
         g: [countyGroup] = {},
         ...rest
-      } = {}
+      } = {},
     } = result;
     const { path: counties = [] } = countyGroup;
+    const {
+      path: [
+        { $: { d: pathBorders } = {} } = {},
+        { $: { d: pathSeparator } = {} } = {},
+      ],
+      metadata: [{ "rdf:RDF": rdf } = {}],
+    } = rest;
+
     const [
-      { $: { d: pathBorders } = {} } = {},
-      { $: { d: pathSeparator } = {} } = {}
-    ] = rest.path;
+      {
+        "cc:Work": [
+          {
+            "cc:license": [{ $: { "rdf:resource": license } = {} } = {}] = [],
+            "dc:creator": [
+              { "cc:Agent": [{ "dc:title": [title] = [] }] = [] } = {},
+            ] = [],
+          },
+        ] = [],
+      },
+    ] = rdf;
 
     const countiesObj = counties
-      .map(({ $: { id, d: path } = {}, title: [title] }) => {
+      .map(({ $: { id, d: path } = {}, title: [{ _: title } = {}] }) => {
         const [name, state] = title.split(",");
 
         return { id, path, name: name.trim(), state: state.trim() };
       })
-      .filter(id => id)
+      .filter((id) => id)
       .reduce((a, { id, path, name, state }) => {
         const cleanId = Number(id.replace(/\D+/g, ""));
 
         a[cleanId] = {
           name: name.trim(),
           state: state.trim(),
-          path
+          path,
         };
 
         return a;
       }, {});
 
-    // Export JS
-    const countiesObjCodeStr = stringify(countiesObj);
-    const codeExport = `
-    // Generated from Wikipedia link. https://upload.wikimedia.org/wikipedia/commons/5/59/Usa_counties_large.svg
+    const jsonHeader = `/*\nCreator: ${title}\nLicense: ${license}\n*/\n\n`;
 
-    const pathBorders = "${pathBorders}";
-    const pathSeparator = "${pathSeparator}";
-    const mapWidth = ${mapWidth};
-    const mapHeight = ${mapHeight};
-
-    export default ${countiesObjCodeStr};
-
-    export {
+    const jsonPayload = JSON.stringify({
       pathBorders,
       pathSeparator,
       mapWidth,
-      mapHeight
-    }
-    `;
-
-    fs.writeFile("usaCounties.js", codeExport, err => {
-      console.log("JS data export. Done.");
+      mapHeight,
+      counties: countiesObj,
     });
 
     // Export JSON
-    fs.writeFile(
-      "usaCounties.json",
-      JSON.stringify({
-        pathBorders,
-        pathSeparator,
-        mapWidth,
-        mapHeight,
-        counties: countiesObj
-      }),
-      err => {
-        console.log("JSON data export. Done.");
-      }
-    );
+    fs.writeFile("usaCounties.json", jsonHeader + jsonPayload, (err) => {
+      console.log("JSON data export. Done.");
+    });
   });
 };
 
-// Use web image
 axios({
   method: "get",
   url,
-  responseType: "text"
+  responseType: "text",
 }).then(({ data }) => {
   processData(data);
 });
-
-// Use a local file
-// fs.readFile("./Usa_counties_large.svg", (err, data) => {
-//   if (err) {
-//     console.log(err);
-//     return;
-//   }
-//   processData(data);
-// });
